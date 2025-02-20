45d8768cf708c1947e29356ed66d966f3812708b
hjb <hjb@hjbrede.de>
2025-02-20 11:33:52 +0100

    Initial Commit

---
+++ TableBug/TableBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableBugApp.swift
+//  TableBug
+//
+//  Created by Hans-J. Brede on 20.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TableBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
